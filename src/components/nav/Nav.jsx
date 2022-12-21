d8378a37f4d7e5018415a8b4acf01fca5e2d9e33
Numan Ahmed <numanahmedh@gmail.com>
2022-12-21 08:35:19 +0530

    Implemented tab bar for navigation

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,15 +4,17 @@ import {AiOutlineHome} from 'react-icons/ai'
 import {BiBookBookmark,BiMessageSquareDetail} from 'react-icons/bi'
 import {RiContactsLine} from 'react-icons/ri'
 import {MdDesignServices} from 'react-icons/md'
+import { useState } from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href='#'><AiOutlineHome/></a>
-      <a href='#about'><RiContactsLine/></a>
-      <a href='#experience'><BiBookBookmark/></a>
-      <a href='#services'><MdDesignServices/></a>
-      <a href='#contact'><BiMessageSquareDetail/></a>
+      <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><RiContactsLine/></a>
+      <a href='#experience'onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BiBookBookmark/></a>
+      <a href='#services'onClick={() => setActiveNav('#services')} className={activeNav === '#services' ? 'active' : ''}><MdDesignServices/></a>
+      <a href='#contact'onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
     </nav>
   )
 }
