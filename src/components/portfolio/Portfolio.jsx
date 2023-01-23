677823c31787fb3acb005c3f1e6c53afec2e7938
Numan Ahmed <numanahmedh@gmail.com>
2023-01-23 21:18:03 +0530

    updated portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,28 +1,33 @@
 import React from 'react'
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio.jpg'
+import IMG2 from '../../assets/portfolio.jpg'
+import IMG1 from '../../components/portfolio/Youtube.webp'
+
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: '1',
-    github: 'https://github.com',
-    demo: 'https:google.com'
+    title: 'Youtube UI',
+    github: 'https://github.com/numan-being/youtube_clone_ui',
+    demo: 'https://youtube-clone-ui-ochre.vercel.app/',
+    description: 'This YouTube UI clone is a replica of the popular video-sharing platform, YouTube. The layout and design of the clone closely mimics that of the original YouTube platform.'
   },
   {
     id: 2,
-    image: IMG1,
+    image: IMG2,
     title: '2',
     github: 'https://github.com',
-    demo: 'https:google.com'
+    demo: 'https:google.com',
+    description: ''
   },
   {
     id: 3,
-    image: IMG1,
+    image: IMG2,
     title: '3',
     github: 'https://github.com',
-    demo: 'https:google.com'
+    demo: 'https:google.com',
+    description: ''
   },
 ]
 
@@ -39,13 +44,16 @@ const Portfolio = () => {
 
       <div className="container portfolio__container">
         {
-          data.map(({id, image, title, github, demo})=> {
+          data.map(({id, image, title, github, demo, description})=> {
             return (
               <article key={id} className='portfolio__item'>
                 <div className='portfolio__item-image'>
                   <img src={image} alt={title} />
                 </div>
                 <h3>{title}</h3>
+                <div className="p-tag">
+                  <p>{description}</p>
+                </div>
                 <div className="portfolio__item-cta">
                   <a href={github} className='btn'>Github</a>
                   <a href={demo} className='btn btn-primary' target='_blank' rel="noreferrer" >Live Demo</a>
